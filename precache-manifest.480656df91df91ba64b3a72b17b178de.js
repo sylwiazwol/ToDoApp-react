c22793fcd361bf70f72dcf7ef95539ffdb1d5062
sylwiazwol <48494412+sylwiazwol@users.noreply.github.com>
2019-09-05 14:03:28 +0200

    Updates

---
+++ precache-manifest.480656df91df91ba64b3a72b17b178de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64e041082c3cc981867",
+    "url": "/ToDoApp-react/static/js/runtime~main.f64e0410.js"
+  },
+  {
+    "revision": "33432fd89ea7ffcb2e09",
+    "url": "/ToDoApp-react/static/js/main.33432fd8.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/ToDoApp-react/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "33432fd89ea7ffcb2e09",
+    "url": "/ToDoApp-react/static/css/main.2a1845ef.chunk.css"
+  },
+  {
+    "revision": "4486fb374f8dc36feb8915f00887f588",
+    "url": "/ToDoApp-react/index.html"
+  }
+];
